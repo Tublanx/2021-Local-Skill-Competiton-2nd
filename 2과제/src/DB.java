69601b5fd45da7309d639bc46c1fdc29b2b9b49d
Tublanx <rlgus03453@naver.com>
2022-03-08 19:01:20 +0900

    데이터베이스 설정

--- 2과제/src/DB.java
+++ 2과제/src/DB.java
@@ -3,15 +3,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import javax.swing.JOptionPane;
-
 public class DB {
 	static Connection con;
 	static Statement stmt;
 
 	static {
 		try {
-			con = DriverManager.getConnection("jdbc:mysql://localhost?serverTimezone=UTC&allowLoadLocalInfile=true",
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost?serverTimezone=UTC&allowPublicKeyRetreval=true&allowLoadLocalInfile=true",
 					"root", "1234");
 			stmt = con.createStatement();
 		} catch (SQLException e) {
@@ -24,8 +23,8 @@ public class DB {
 		try {
 			stmt.execute(sql);
 		} catch (SQLException e) {
-			JOptionPane.showMessageDialog(null, "¼ÂÆÃ ½ÇÆÐ", "°æ°í", 0);
-			System.exit(0);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
@@ -36,19 +35,19 @@ public class DB {
 	}
 
 	public DB() {
-		execute("drop database if exists 2022Áö¹æ_1");
-		execute("create database 2022Áö¹æ_1 default character set utf8");
+		execute("drop database if exists 2022ì§€ë°©_1");
+		execute("create database 2022ì§€ë°©_1 default character set utf8");
 		execute("drop user if exists user@localhost");
 		execute("create user user@localhost identified by '1234'");
-		execute("grant insert,select,update,delete on 2022Áö¹æ_1.* to user@localhost");
+		execute("grant select, update, insert, delete on 2022ï¿½ï¿½ï¿½ï¿½_1. *to user@localhost");
 		execute("set global local_infile=1");
-		execute("use 2022Áö¹æ_1");
+		execute("use 2022ì§€ë°©_1");
 
 		createT("area", "a_no int primary key not null auto_increment, a_name varchar(2)");
 		createT("cafe",
 				"c_no varchar(10) primary key, c_name varchar(20), t_no varchar(100), c_tel varchar(15), a_no int, c_address varchar(50), c_price int, foreign key(a_no) references area(a_no)");
 		createT("genre", "g_no int primary key not null auto_increment, g_name varchar(10)");
-		createT("map", "a_no int, m_x int, m_y int, foreign key(a_no) references area(a_no)");
+		createT("map", "a_no int,m_x int, m_y int, foreign key(a_no) references area(a_no)");
 		createT("ping", "a_no int, p_x int, p_y int, foreign key(a_no) references area(a_no)");
 		createT("user",
 				"u_no int primary key not null auto_increment, u_id varchar(10), u_pw varchar(10), u_name varchar(10), u_date date");
@@ -56,12 +55,10 @@ public class DB {
 				"n_no int primary key not null auto_increment, u_no int, n_date date, n_title varchar(20), n_content varchar(150), n_viewcount int, n_open int, foreign key(u_no) references user(u_no)");
 		createT("quiz", "q_no int primary key not null auto_increment, q_answer varchar(10)");
 		createT("theme",
-				"t_no int primary key not null auto_increment, t_name varchar(30), g_no int, t_explan varchar(200), t_personal int, t_time int, foreign key(g_no) references genre(g_no)");
+				"t_no int primary key not null auto_increment, t_name varchar(30), g_no int, t_explan varchar(200), t_personnel int, t_time int, foreign key(g_no) references genre(g_no)");
 		createT("reservation",
 				"r_no int primary key not null auto_increment, u_no int, c_no varchar(10), t_no int, r_date date, r_time varchar(20), r_people int, r_attend int, foreign key(u_no) references user(u_no), foreign key(c_no) references cafe(c_no), foreign key(t_no) references theme(t_no)");
 
-		JOptionPane.showMessageDialog(null, "¼ÂÆÃ ¼º°ø", "Á¤º¸", 1);
-		System.exit(0);
 	}
 
 	public static void main(String[] args) {
