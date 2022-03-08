5ed009e1063bf4869592228ff37d72e09d130604
Tublanx <rlgus03453@naver.com>
2022-03-08 13:50:01 +0900

    폼 기본 세팅

--- 2과제/src/Baseframe.java
+++ 2과제/src/Baseframe.java
@@ -1,19 +1,28 @@
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 
 public class Baseframe extends JFrame {
 
 	static Connection con = DB.con;
 	static Statement stmt = DB.stmt;
+
+	static DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
 
 	static {
 		try {
@@ -73,6 +82,58 @@ public class Baseframe extends JFrame {
 		var b = new JButton(t);
 		b.addActionListener(a);
 		return b;
+	}
+
+	<T extends JComponent> T sz(T c, int w, int h) {
+		c.setPreferredSize(new Dimension(w, h));
+		return c;
+	}
+
+	DefaultTableModel model(String[] col) {
+		var m = new DefaultTableModel(null, col) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+
+		return m;
+	}
+
+	JTable table(DefaultTableModel m) {
+		var t = new JTable(m);
+
+		t.getTableHeader().setReorderingAllowed(false);
+		t.getTableHeader().setResizingAllowed(false);
+
+		t.setSelectionMode(0);
+
+		for (int i = 0; i < t.getColumnCount(); i++) {
+			t.getColumnModel().getColumn(i).setCellRenderer(dtcr);
+		}
+
+		return t;
+	}
+
+	public Baseframe(String t, int w, int h) {
+		super(t);
+		this.setSize(w, h);
+		this.setDefaultCloseOperation(2);
+		this.setLocationRelativeTo(null);
+	}
+
+	class Before extends WindowAdapter {
+		Baseframe b;
+
+		public Before(Baseframe b) {
+			this.b = b;
+			b.setVisible(false);
+		}
+
+		@Override
+		public void windowClosed(WindowEvent e) {
+			b.setVisible(true);
+		}
 	}
 
 }
